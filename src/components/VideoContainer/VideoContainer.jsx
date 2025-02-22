abaeea47215f97dd05a62e262940cc9b20ba686f
lucasluccaroni <lucas_luccaroni@hotmail.com.ar>
2025-02-22 12:12:04 -0300

    agregando cfg del video

--- src/components/VideoContainer/VideoContainer.jsx
+++ src/components/VideoContainer/VideoContainer.jsx
@@ -4,8 +4,7 @@ const VideoContainer = () => {
     return (
         <div className={css.videoContainer} >
             {/* <iframe className={css.video} src="https://www.youtube.com/embed/3YTohytF9oE?si=_KPfIwxAGDlzPwdN&amp?autoplay=1&controls=0" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin"></iframe> */}
-            <iframe className={css.video} src="https://www.youtube.com/embed/EDjb2nLSxDo?autoplay=1&controls=0&muted=1&vq=hd1440" allow="autoplay;encrypted-media
-            // "></iframe>
+            <iframe className={css.video} src="https://www.youtube.com/embed/EDjb2nLSxDo?autoplay=1&controls=0&muted=1&vq=hd1440" allow="autoplay;encrypted-media"></iframe>
         </div>
     )
 }
