e122927017367451c793701e02a609826455cdf9
LUCAS JOSE LUCCARONI <lucas.luccaroni@vicentelopez.gov.ar>
2025-02-28 13:08:03 -0300

    cargando productos en db

--- src/components/ButtonHamb/ButtonHamb.jsx
+++ src/components/ButtonHamb/ButtonHamb.jsx
@@ -7,14 +7,14 @@ const ButtonHamb = () => {
             <div className={css.menuContainer}>
                 <Link className={css.link} to="/productos" > Productos </Link>
                 <div className={css.menuContent}>
-                    <Link to={"/category/sofas"} > Sofas</Link>
+                    <Link to={"/category/sofas"} > Sofás</Link>
                     <Link to={"/category/esquineros"} > Esquineros</Link>
                     <Link to={"/category/poltronas"} > Poltronas </Link>
                     <Link to={"/category/mesas"} > Mesas </Link>
                     <Link to={"/category/mesas-ratonas"} >Mesas Ratonas</Link>
                     <Link to={"/category/sillas"} > Sillas </Link>
                     <Link to={"/category/racks-y-vajilleros"} > Racks y Vajilleros</Link>
-                    <Link to={"/category/comodas-y-mesas-luz"} > Comodas y Mesas de Luz </Link>
+                    <Link to={"/category/comodas-y-mesas-luz"} > Cómodas y Mesas de Luz </Link>
                     <Link to={"/category/escritorios-y-estanterias"} > Escritorios y Estanterias </Link>
                     <Link to={"/category/puffs"} > Puffs </Link>
                     <Link to={"/category/ilumninacion"} > Ilumniación </Link>
