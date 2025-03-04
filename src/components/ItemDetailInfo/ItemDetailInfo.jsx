9c57428061fb8bb73c88b955fe81eb84c4fe4a02
lucasluccaroni <lucas_luccaroni@hotmail.com.ar>
2025-03-04 02:20:59 -0300

    Services hecho + responsive. Ajustes de estilado general

--- src/components/ItemDetailInfo/ItemDetailInfo.jsx
+++ src/components/ItemDetailInfo/ItemDetailInfo.jsx
@@ -8,24 +8,27 @@ const ItemDetailInfo = ({ name, description1, description2, description3 }) => {
                 <h1 className={css.name} > {name} </h1>
                 <hr className={css.line} />
             </div>
+
             <div className={css.descriptions} >
                 <h3> Características </h3>
                 <hr className={css.line} />
+
                 <div className={css.text} >
                     <p className={css.description} > -  {description1}</p>
                     <p className={css.description} > -  {description2}</p>
                     <p className={css.description} > -  {description3}</p>
                 </div>
+
             </div>
 
             <div className={css.linksContainer} >
+
                 <a className={css.boton} href="https://wa.me/5491162763002" target="_blank" rel="noopener noreferrer">
-                    {/* <img className={css.wpLogo} src="/images/redes/whatsapp.png" alt="Whatsapp logo" /> */}
-                    <p /* className={css.linkText} */ > ¡Lo quiero! </p>
+                    <p className={css.linkText} > ¡Lo quiero! </p>
                 </a>
+
                 <a className={css.boton} href="/test.pdf" target="_blank" rel="noopener noreferrer">
-                    {/* <img className={css.wpLogo} src="/images/redes/pdf.png" alt="PDF logo" /> */}
-                    <p /* className={css.linkText} */ > Lista de precios </p>
+                    <p className={css.linkText} > Lista de precios </p>
                 </a>
 
             </div>
--- src/components/ServicesView/ServicesView.jsx
+++ src/components/ServicesView/ServicesView.jsx
@@ -1,11 +1,11 @@
 import { useEffect } from "react"
-import { Link } from "react-router-dom"
-import ServicesComponent from "../ServicesComponent/ServicesComponent"
+import Services from "../Services/Services"
 import css from "./ServicesView.module.css"
 import NavigateButtons from "../NavigateButtons/NavigateButtons"
 
 
 const ServicesView = () => {
+
     useEffect(() => {
         document.title = "Parben Home | Servicios"
         window.scrollTo({ top: 0, behavior: "instant" })
@@ -13,13 +13,9 @@ const ServicesView = () => {
 
     return (
         <div className={css.container}>
-            {/* <Link className={css.navigateButton} to={`/`} > Volver </Link> */}
             <NavigateButtons/>
-            <h1 className={css.subTitulo} >Los servicios que te ofrecemos.</h1>
-            <ServicesComponent variant={0} imagen={3} />
-            <ServicesComponent variant={1} imagen={3} reverse={true} />
-            <ServicesComponent variant={2} imagen={3} />
-
+            <h1 className={css.titulo} >Los servicios que te ofrecemos</h1>
+            <Services/>
         </div>
     )
 }
