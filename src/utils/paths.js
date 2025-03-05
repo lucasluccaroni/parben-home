a8807b4710812ce7851f7b05299ff6da91bb3309
LUCAS JOSE LUCCARONI <lucas.luccaroni@vicentelopez.gov.ar>
2025-03-05 15:11:49 -0300

    corrigo paths

--- src/utils/paths.js
+++ src/utils/paths.js
@@ -1,12 +1,12 @@
 
 export const paths = [
-    { path: "/category/sofas", pathName: "Sofas", key: "1", img: "/images/products-category/sofas.png" },
+    { path: "/category/sofas", pathName: "Sofás", key: "1", img: "/images/products-category/sofas.png" },
     { path: "/category/esquineros", pathName: "Esquineros", key: "2", img: "/images/products-category/esquineros.jpg" },
     { path: "/category/poltronas", pathName: "Poltronas", key: "3", img: "/images/products-category/poltronas.png" },
     { path: "/category/mesas", pathName: "Mesas", key: "4", img: "/images/products-category/mesas.png" },
     { path: "/category/mesas-ratonas", pathName: "Mesas Ratonas", key: "5", img: "/images/products-category/ratonas.png" },
     { path: "/category/sillas", pathName: "Sillas", key: "6", img: "/images/products-category/sillas.png" },
-    { path: "/category/racks-y-vajileros", pathName: "Racks y Vajilleros", key: "7", img: "/images/products-category/racks-vajilleros.png" },
+    { path: "/category/racks-y-vajilleros", pathName: "Racks y Vajilleros", key: "7", img: "/images/products-category/racks-vajilleros.png" },
     { path: "/category/comodas-y-mesas-luz", pathName: "Cómodas y Mesas de Luz", key: "8", img: "/images/products-category/comodas-mesas-luz.png" },
     { path: "/category/escritorios-y-estanterias", pathName: "Escritorios y Estanterías", key: "9", img: "/images/products-category/escritorios-estanterias.png" },
     { path: "/category/puffs", pathName: "Puffs", key: "10", img: "/images/products-category/puffs.png" },
