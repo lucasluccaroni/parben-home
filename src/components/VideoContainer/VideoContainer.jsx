ca3e885aa29016bbf59c90d35d39c9aed5e7aef1
LUCAS JOSE LUCCARONI <lucas.luccaroni@vicentelopez.gov.ar>
2025-03-11 11:12:29 -0300

    video final con fades y reducido por git lfs

--- src/components/VideoContainer/VideoContainer.jsx
+++ src/components/VideoContainer/VideoContainer.jsx
@@ -1,34 +1,16 @@
 import css from "./VideoContainer.module.css"
-import { useRef } from "react"
 
 const VideoContainer = () => {
-
-    const videoRef = useRef(null)
-
-    const handleVideoRef = () => {
-        const videoElement = videoRef.current
-        if (videoElement) {
-            videoElement.classList.add("fade-out")
-
-            setTimeout(() => {
-                videoElement.classList.remove("fade-out");
-                videoElement.currentTime = 0; // Reinicia el tiempo del video
-                videoElement.play(); // Reproduce nuevamente
-            }, 1000);
-        }
-    }
 
     return (
         <div className={css.videoContainer} >
 
             <video
-                ref={videoRef}
                 className={css.video}
-                src="/video/home-video1-reduced.mp4"
+                src="/video/home-video1-reduced-fades-final.mp4"
                 autoPlay
                 muted
                 loop
-                // onEnded={handleVideoRef}
                 style={{ width: "100%" }}
             />
 
